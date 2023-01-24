015e22204a67769d576d2664229a93fbde8ca677
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-24 12:06:40 -0300

    feat: req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,25 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(data: list):
+        data_de_produção = str(date.max)
+        validade_mais_proxima = str(date.max)
+
+        empresa_com_mais_produtos = Counter(
+            [product["nome_da_empresa"] for product in data]
+        ).most_common(1)[0][0]
+
+        for product in data:
+            if product["data_de_fabricacao"] < data_de_produção:
+                data_de_produção = product["data_de_fabricacao"]
+
+            if product["data_de_validade"] < validade_mais_proxima:
+                validade_mais_proxima = product["data_de_validade"]
+        return (
+            f"Data de fabricação mais antiga:{data_de_produção}\n"
+            f"Data de validade mais próxima: {validade_mais_proxima}\n"
+            f"Empresa com mais produtos: {empresa_com_mais_produtos}"
+        )
