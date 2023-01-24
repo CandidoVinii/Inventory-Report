99b1c8633195f28375c29f07c6a103e25ed6014c
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-24 12:06:34 -0300

    feat: req 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, "Caneca", "Fallen", "18-01-2023", "18-01-2023", "1800", "armário"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Caneca"
+    assert product.nome_da_empresa == "Fallen"
+    assert product.data_de_fabricacao == "18-01-2023"
+    assert product.data_de_validade == "18-01-2023"
+    assert product.numero_de_serie == "1800"
+    assert product.instrucoes_de_armazenamento == "armário"
