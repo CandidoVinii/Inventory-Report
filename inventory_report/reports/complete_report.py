4494835cdd13e44a51255c84f109b22f93ce2ef8
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-01-24 12:06:47 -0300

    feat: req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
+from inventory_report.reports.simple_report import SimpleReport
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(data: list):
+        simple_Report = SimpleReport.generate(data)
+        products_industry = {}
+
+        for product in data:
+            if product["nome_da_empresa"] in products_industry:
+                products_industry[product["nome_da_empresa"]] += 1
+            else:
+                products_industry[product["nome_da_empresa"]] = 1
+
+        companies = ""
+        for company, quantity in products_industry.items():
+            companies += f"- {company}: {quantity}\n"
+
+        return (
+            f"{simple_Report}\n"
+            f"Produtos estocados por empresa:\n{companies}"
+        )
